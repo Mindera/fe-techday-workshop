a5aa7510a4ccead60ae1b4d3135a4fa17f3550c3
Gil Filipe <gil.filipe@mindera.com>
2018-05-24 20:10:12 +0100

    Added functions

---
+++ app/functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+app.get('/notify', (request, response) => {
+  response.send('Notify Luis');
+  notifyMe('project changed');
+});
+
+exports.app = functions.https.onRequest(app);
+
+function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+}

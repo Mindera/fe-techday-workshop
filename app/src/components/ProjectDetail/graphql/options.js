1dd822b83fa8264b3b8f21668fd835260de67e9d
Luís Almeida <luis.almeida@mindera.com>
2018-05-25 18:19:56 +0100

    project detail component ready for workshop attendees

--- app/src/components/ProjectDetail/graphql/options.js
+++ app/src/components/ProjectDetail/graphql/options.js
@@ -1,14 +1,11 @@
 export default {
   options: (props) => {
-    return {
-      fetchPolicy: "cache-and-network",
-      variables: {
-        id: props.match.params.id
-      }
-    };
+    // this function gets the component initial Props (here you may get the project id)
+    // and must return an object with the query variables (such as the project id)
   },
 
   props: (result) => {
-    return { ...result.data.project, isLoading: result.data.loading };
+    // here you get the raw result that the graph gave us, so you can deconstruct it
+    // and build the props structure you want to return back to the component
   }
 };
